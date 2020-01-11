895663fc1c2b87b58305e99877ddb1627e5e676b
DELL <604957104@qq.com>
2020-01-11 21:08:00 +0800

    1

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/GmallPmsApplication.java
@@ -1,9 +1,13 @@
 package com.atguigu.gmall.pms;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.atguigu.gmall.pms.mapper")
+@EnableDubbo
 public class GmallPmsApplication {
 
     public static void main(String[] args) {
--- gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
@@ -1,13 +1,33 @@
 package com.atguigu.gmall.pms;
 
+import com.atguigu.gmall.pms.entity.Brand;
+import com.atguigu.gmall.pms.service.BrandService;
+import com.atguigu.gmall.pms.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class GmallPmsApplicationTests {
 
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    BrandService brandService;
     @Test
     void contextLoads() {
+
+//        Product byId = productService.getById(1);
+//        System.out.println(byId.getName());
+//        Brand brand = new Brand();
+//        brand.setName("heheh");
+//        brandService.save(brand);
+
+        Brand brand = brandService.getById(53);
+        System.out.println(brand.getName());
     }
 
 }
